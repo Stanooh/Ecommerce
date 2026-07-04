d8607a7a85c7cceb6fa57b6a6f7f2e723ee3c80f
Stanooh <kimanistanley6741@gmail.com>
2026-07-04 11:11:39 +0300

    Create integration test for HomePage

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -32,7 +32,7 @@ setTimeout(()=>{
 
 
     return(
-           <div className="product-container">
+           <div className="product-container" data-testid="product-container">
                     <div className="product-image-container">
                       <img className="product-image" data-testid="product-image" src={product.image} />
                     </div>
